54f5f2985612351edbe7cacdbc1f00f7c7a3f215
nice try <1609909764@qq.com>
2026-02-27 09:41:58 +0800

    修改

--- src/api/github.ts
+++ src/api/github.ts
@@ -1,6 +1,6 @@
 const OWNER = 'xh-0'
 const REPO = 'plan-hu'
-const TOKEN = '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'
+const TOKEN = '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'
 
 const BASE = `https://api.github.com/repos/${OWNER}/${REPO}`
 
